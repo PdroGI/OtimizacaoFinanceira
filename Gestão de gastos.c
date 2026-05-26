3574cf07bc39b243c1acbb6514c41e5a931496c6
Pedro Guimarães Ignácio <pedro2018pepe@gmail.com>
2026-05-26 15:33:19 -0300

    Update Gestão de gastos.c

--- Gestão de gastos.c
+++ Gestão de gastos.c
@@ -19,7 +19,7 @@ int main() {
                             for (int i = 0; i < n; i++) {
                                  printf("\nDespesa %d\n", i + 1);
                                  printf("Nome:");
-                                 scanf("%s", nomes[i]);
+                                 scanf("%[^/n]s", nomes[i]);
                                  printf("Valor: R$ ");
                                  scanf("%f", &valores[i]);
                                  printf("Pode cortar? (1=Sim / 0=Nao): ");
